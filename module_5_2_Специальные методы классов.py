7256bd2ac6fba71bc1daf9bd51f2e9d8c94ad00c
Yulya2809 <93946203+Yulya2809@users.noreply.github.com>
2024-09-08 21:52:28 +0300

    Add files via upload

---
+++ module_5_2_Специальные методы классов.py
@@ -0,0 +1,31 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        floor = 0
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print(f'{new_floor} - такого этажа не существует')
+        else:
+            for floor in range(new_floor):
+                print(floor + 1)
+
+    def __len__(self):
+        return (self.number_of_floors)
+
+    def __str__(self):
+        i = str(f'Название: {self.name}, кол-во этажей:{self.number_of_floors}')
+        return i
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
