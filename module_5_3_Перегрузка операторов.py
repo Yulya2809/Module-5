dc87570326f40fe537b607bf5e9867fa58209d74
Yulya2809 <93946203+Yulya2809@users.noreply.github.com>
2024-09-10 20:36:37 +0300

    Add files via upload

--- module_5_3_Перегрузка операторов.py
+++ module_5_3_Перегрузка операторов.py
@@ -17,22 +17,28 @@ class House:
         return f'Название: {self.name}, кол-во этажей:{self.number_of_floors}'
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+         return self.number_of_floors <= other.number_of_floors
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
 
     def __add__(self, value):
         self.number_of_floors = self.number_of_floors + value
