43f9791a60bf2812cbda413880f32e7a0a0d2066
Yulya2809 <93946203+Yulya2809@users.noreply.github.com>
2024-09-24 19:16:04 +0300

    Add files via upload

--- module_5_Доп.задание.py
+++ module_5_Доп.задание.py
@@ -41,9 +41,42 @@ class UrTube:
                 new_video = Video(title, duration, time_now, adult_mode)
                 self.videos.append(new_video)
 
-    #def get_video(self):
-    #
-    # def watch_video(self):
+    def get_video(self, word):
+        result = []
+        for title in self.title:
+            if word.lower() in title.lower():
+                result.append(title)
+            return result
+
+    def watch_video(self, title):
+        for name_video in self.title:
+            if name_video == title:
+                self.duration = name_video
+                self.time_now = 0
 
 
+ur = UrTube()
+v1 = Video('Лучший язык программирования 2024 года', 200)
+v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+
+# Добавление видео
+ur.add(v1, v2)
+
+# Проверка поиска
+print(ur.get_videos('лучший'))
+print(ur.get_videos('ПРОГ'))
+
+# Проверка на вход пользователя и возрастное ограничение
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('vasya_pupkin', 'lolkekcheburek', 13)
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+ur.watch_video('Для чего девушкам парень программист?')
+
+# Проверка входа в другой аккаунт
+ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+print(ur.current_user)
+
+# Попытка воспроизведения несуществующего видео
+ur.watch_video('Лучший язык программирования 2024 года!')
 
